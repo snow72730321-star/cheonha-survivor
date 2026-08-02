8cb2245328f0ce23603b96c1c61b4ec3ced274ed
Chanyu Kim <aqu7273@gachon.ac.kr>
2026-08-02 20:40:10 +0900

    patch

--- cheonha-survivor-v14/js/core/pwa.js
+++
@@ -1,2 +0,0 @@
-"use strict";
-if("serviceWorker" in navigator&&location.protocol.startsWith("http")){addEventListener("load",()=>navigator.serviceWorker.register("service-worker.js").catch(()=>{}));}
