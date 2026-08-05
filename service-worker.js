4c9717771699854fe63cc28e4f176c19d4cdce8d
Chanyu Kim <aqu7273@gachon.ac.kr>
2026-08-05 18:59:31 +0900

    patch

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,7 @@
 "use strict";
 
-/** v14.3.10 캐릭터 프레임 안전 여백 패치 오프라인 캐시. 대용량 BGM은 스트리밍하고 SFX는 최초 사용 뒤 캐시한다. */
-const CACHE="cheonha-v14-3-14-sprite-recenter-feet";
+/** v14.3.15 오디오·VFX 누락 복구 오프라인 캐시. 대용량 BGM은 스트리밍하고 SFX는 최초 사용 뒤 캐시한다. */
+const CACHE="cheonha-v14-3-15-audio-vfx-recovery";
 const APP_SHELL=[
   "./","index.html","manifest.webmanifest",
   "css/base.css","css/systems.css","css/remaster.css","css/mobile.css","css/animation-pass.css","css/awakening-cutscene.css","css/forge-v13.css","css/v14-improvements.css","css/audio-mixer-v14-3-3.css",
